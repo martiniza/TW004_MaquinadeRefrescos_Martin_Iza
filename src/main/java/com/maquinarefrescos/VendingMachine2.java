4b3a56880792336dff103363970cf10ba094426d
martiniza <93280991+martiniza@users.noreply.github.com>
2021-11-02 21:48:51 +0100

    update arrays
    comenzamos implementacion de arrays en el código

--- src/main/java/com/maquinarefrescos/VendingMachine2.java
+++ src/main/java/com/maquinarefrescos/VendingMachine2.java
@@ -1,11 +1,18 @@
 package com.maquinarefrescos;
 
+import com.maquinaexceptions.SinCambiosException;
+import com.maquinaexceptions.SinStockException;
+
 public class VendingMachine2 {
 
 	// variables
- Refresco refresco;
+
+	Refresco[] misrefrescos = {
+			new Refresco(20,80,"Agua"),
+			new Refresco(10,120,"Zumo"),
+			new Refresco(10,120,"Cola")};
 	double dineroIng;
-	
+
 	// Estado de la máquina
 
 	private double cambios;
@@ -15,58 +22,61 @@ public class VendingMachine2 {
 	private int refrescosVendidos;
 
 	// Constructores
+	
 
 	public VendingMachine2(Refresco[] refresco, double cambiosIni) {
 		super();
 		this.dineroRecaudado = 0;
 		this.refrescosVendidos = 0;
+		this.misrefrescos = refresco;
 		this.cambios = cambiosIni;
 	}
-	
-	Refresco[] misrefrescos = {
-			new Refresco(20,80,"Agua"),
-			new Refresco(10,120,"Zumo"),
-			new Refresco(10,120,"Cola")};
-	
-	VendingMachine2 Maquina = new VendingMachine2(misrefrescos, 3000);
 
 	// Metodos
 
-	public double vender(Refresco refresco, double dineroIng) {
+	public double vender(int i, double dineroIng) throws SinStockException, SinCambiosException {
 
-		double cambioEsperado = dineroIng - refresco.getPrecio();
+		double cambioEsperado = dineroIng - misrefrescos[i].getPrecio();
+		double cambioNecesario = misrefrescos[i].getPrecio()- dineroIng;
 
-		if (refresco.getUnidades() <= 0) {
-			return -1; 									//excepción sin Stock
-		} else if (this.cambios < cambioEsperado) {
-			return -2; 									//excepción sin cambios
-		} else if (dineroIng <= refresco.getPrecio()) {
-			return -3; 									//excepción dinero insuficiente
+		if (misrefrescos[i].getUnidades() <= 0) {
+			throw new SinStockException("Error. No hay Stock disponible"); 
+			// excepción sin Stock
+		} 
+		
+		else if (this.cambios < cambioEsperado) {
+			throw new SinCambiosException("Error. No hay cambios disponibles"); 
+			// excepción sin cambios
+		} 
+		
+		else if (dineroIng <= misrefrescos[i].getPrecio()) {
+			throw new SinCambiosException("Error. Falta dinero", cambioNecesario); 
+			// excepción dinero insuficiente
 		} else {
 
-			--refresco.unidadesStock;					// Cambios en el Stock
+			--misrefrescos[i].unidadesStock; // Cambios en el Stock
 
-			cambios -= cambioEsperado;					// Cambio en los cambios
+			cambios -= cambioEsperado;
 
-			++refrescosVendidos;						// Cambio en estado de maquina
-			dineroRecaudado += refresco.getPrecio();
+			++refrescosVendidos; // Cambio en estado de maquina
+			dineroRecaudado += misrefrescos[i].getPrecio();
 
 			return cambioEsperado;
 		}
 	}
 
 	public String informeVentas() {
-		return "La cantidad de refrescos vendida es" + refrescosVendidos +
-				"; El dinero recaudado es "+ dineroRecaudado;
+		return "La cantidad de refrescos vendida es" + refrescosVendidos + "; El dinero recaudado es "
+				+ dineroRecaudado;
 	}
 
 	public String estadoMaquina(Refresco refresco) {
-		return "La cantidad de refrescos restante es " + refresco.getUnidades() +
-				"; La cantidad de cambio restante es " + cambios;
+		return "La cantidad de refrescos restante es " + refresco.getUnidades() + "; La cantidad de cambio restante es "
+				+ cambios;
 	}
 
-	public Refresco getRefresco() {
-		return refresco;
+	public Refresco getRefresco(int i) {
+		return misrefrescos[i];
 	}
 
 	public double getDineroIng() {
@@ -87,7 +97,6 @@ public class VendingMachine2 {
 
 	// Aquí esta la ejecución del main class
 	public static void main(String[] args) {
-
 
 	}
 
